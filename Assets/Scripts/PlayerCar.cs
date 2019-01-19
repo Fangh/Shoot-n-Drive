e499cfb996463fbf658859a52afe68a25ffe75e7
Fangh <toto.klein@laposte.net>
2019-01-19 20:30:54 +0100

    Fake cockpit + fake steerwheel + move forward

---
+++ Assets/Scripts/PlayerCar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerCar : MonoBehaviour
+{
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float acceleration;
+
+    private float currentSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentSpeed < maxSpeed)
+            currentSpeed += acceleration * Time.deltaTime;
+
+        transform.Translate(transform.forward * currentSpeed * Time.deltaTime);
+    }
+}
