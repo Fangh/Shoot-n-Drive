3967f82d23170cd3f4298d6f87f4d6cb906bfd89
KaijuGirl <l.gatay@laposte.net>
2019-01-20 12:21:19 +0100

    Ajout du gun et du script movePlayer

---
+++ Assets/Scripts/movePlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePlayer : MonoBehaviour
+{
+
+    public float axeGaucheDroite;
+    public float axeFreinage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        axeFreinage = Input.GetAxis("Vertical");
+        if(axeFreinage >= 0)
+        {
+            axeFreinage = 0;
+        }
+
+        if (axeFreinage != -1)
+        {
+            axeGaucheDroite = Input.GetAxis("Horizontal");
+        }
+        transform.Translate(axeGaucheDroite, 0, axeFreinage + 1);
+    }
+}
